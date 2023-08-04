c084d74e252013666dfeabe03a59cb7a2df53f6b
omarzayed7777 <141397052+omarzayed7777@users.noreply.github.com>
2023-08-04 10:59:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, Response
+from utils import chatgpt, diffusion
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create-article', methods=['GET', 'POST'])
+def create_article():
+    if request.method == 'GET':
+        return render_template('create-article.html')
+    
+    
+@app.route('/summarize-article', methods=['GET', 'POST'])
+def summarize_article():
+    if request.method == 'GET':
+        return render_template('summarize-article.html')
+    
+@app.route('/gpt-api', methods=['POST'])
+def gpt_api():
+    system_settings = request.json.get('system_settings')
+    user_input = request.json.get('user_input')
+    return Response(chatgpt(system_settings, user_input))
+
+@app.route('/sd-api', methods=['POST'])
+def sd_api():
+    prompt = request.json.get('prompt')
+    return {
+        'imageUrl': "data:image/png;base64, " + diffusion(prompt)
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,54 @@
+import openai
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_KEY')
+ENGINE_ID = 'stable-diffusion-xl-1024-v1-0'
+API_HOST = 'https://api.stability.ai'
+stablediffusion_api_key = os.getenv('DREAMSTUDIO_KEY')
+
+def chatgpt(system_settings, user_input):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": system_settings}, {"role": "user", "content": user_input}],
+        temperature=0,
+        stream=True
+    )
+    for x in response:
+        if x.choices[0].delta.get('content'):
+            yield x.choices[0].delta.content
+
+def diffusion(prompt):
+    response = requests.post(
+    f"{API_HOST}/v1/generation/{ENGINE_ID}/text-to-image",
+    headers={
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {stablediffusion_api_key}"
+    },
+    json={
+            "text_prompts": [
+                {
+                    "text": prompt
+                }
+            ],
+            "cfg_scale": 7,
+            "height": 1024,
+            "width": 1024,
+            "samples": 1,
+            "steps": 30,
+        },
+    )
+
+    if response.status_code != 200:
+        raise Exception("Non-200 response: " + str(response.text))
+
+    data = response.json()
+    output = ''
+    for i, image in enumerate(data["artifacts"]):
+        # Get the base64 string from the image dictionary
+        base64_string = image["base64"]
+        output += base64_string
+    return output
