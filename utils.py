6bf49f7a0cb0a4f514e6f4ec1e39caf4cdc9281c
Omar Zayed <omarzayed7777@gmail.com>
2023-08-16 11:38:32 +0300

    fixed an error

--- utils.py
+++ utils.py
@@ -6,9 +6,10 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chat_models import ChatOpenAI
+load_dotenv()
+
 loader = TextLoader('questions.txt', encoding='utf-8')
 index = VectorstoreIndexCreator().from_loaders([loader])
-load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 ENGINE_ID = 'stable-diffusion-xl-1024-v1-0'
 API_HOST = 'https://api.stability.ai'
